eef64e2107b473a97155564d0fd9011f1dbc3ed7
Benjamin Zhang <bzh@ocf.berkeley.edu>
2019-03-02 16:55:25 -0800

    Add exception logging.

--- ldapsync/ldapsync.py
+++ ldapsync/ldapsync.py
@@ -2,8 +2,11 @@
 """Script for syncing OCF LDAP groups with various services."""
 
 import argparse
+import logging
 import pkgutil
 import traceback
+from ocflib.misc import mail
+
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
@@ -16,12 +19,17 @@ def main():
 
     args = parser.parse_args()
 
+    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(module)s:%(message)s')
+
     for importer, mod_name, _ in pkgutil.iter_modules(['ldapsync/apps']):
         mod = importer.find_module(mod_name).load_module(mod_name)
         try:
             mod.sync(args.dry_run)
         except Exception as e:
             print(traceback.format_exc())
+            logging.exception("Exception caught: ")
+            mail.send_problem_report(f"An exception occurred in ldapsync: \n\n
+                    {traceback.format_exc()}")
 
 if __name__ == '__main__':
     main()
