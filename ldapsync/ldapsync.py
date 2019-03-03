4eed0baf3569730c18ebec8204ad0698c6a617a9
Benjamin Zhang <benzhang4@gmail.com>
2019-03-02 17:58:44 -0800

    Add a handler to logging that writes to stdout, and make all prints go through logging instead.

--- ldapsync/ldapsync.py
+++ ldapsync/ldapsync.py
@@ -4,6 +4,7 @@
 import argparse
 import logging
 import pkgutil
+import sys
 import traceback
 from ocflib.misc import mail
 
@@ -16,17 +17,32 @@ def main():
         action='store_true',
         help='Do not make changes, but show what would be done.'
     )
+    parser.add_argument(
+        '--log-file',
+        '-l',
+        help='Log any changes and any errors to the specified file.'
+    )
 
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(asctime)s:%(levelname)s:%(module)s:%(message)s')
+    logging_handlers = []
+    # Always log to STDOUT.
+    logging_handlers.append(logging.StreamHandler(stream=sys.stdout))
+
+    if (!args.log_file):
+        logging_handlers.append(logging.FileHandler(args.log_file))
+
+    logging.basicConfig(
+        format='%(asctime)s:%(levelname)s:%(module)s:%(message)s',
+        handlers=logging_handlers)
 
     for importer, mod_name, _ in pkgutil.iter_modules(['ldapsync/apps']):
         mod = importer.find_module(mod_name).load_module(mod_name)
         try:
+            # logging functions called in sync() will inherit this one's
+            # options.
             mod.sync(args.dry_run)
         except Exception as e:
-            print(traceback.format_exc())
             logging.exception("Exception caught: ")
             mail.send_problem_report(f"An exception occurred in ldapsync: \n\n
                     {traceback.format_exc()}")
